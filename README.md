ff004d32d8981f599c4bc6284b62f9d64bdf6a5e
lonewolfgames <nathanfaucett@gmail.com>
2014-07-13 14:52:09 -0500

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,4 @@
 ObjectPool
-=====
+=======
 
-object pooling
-
-
-```
-function Vec() {
-	this.x = 0;
-	this.y = 0;
-}
-
-var VecPool = new ObjectPool(Vec), // creare new ObjectPool with constructor
-	vec = VecPool.create();
-
-
-vec.x = 10;
-vec.y = 25;
-
-// removes object and pool it
-VecPool.removeObject(vec);
-
-// removes all objects in arguments and pools them
-VecPool.remove(vec, vec1, vec2);
-
-// removes all objects and pools them
-VecPool.clear();
-
-```
+Object pooling for the browser and node.js
--- src/index.js
+++ src/index.js
@@ -2,51 +2,78 @@
 
 function ObjectPool(constructor) {
 
-	this.pooled = [];
-	this.objects = [];
-	this.object = constructor;
+    this.pooled = [];
+    this.objects = [];
+    this.constructor = constructor;
 }
 
 
 ObjectPool.prototype.create = function() {
-	var pooled = this.pooled,
-		object = pooled.length ? pooled.pop() : new this.object;
-
-	this.objects.push(object);
-	return object;
+    var pooled = this.pooled,
+        object = pooled.length ? pooled.pop() : new this.constructor;
+    
+    this.objects.push(object);
+    return object;
 };
 
 
 ObjectPool.prototype.removeObject = function(object) {
-	var objects = this.objects,
-		pooled = this.pooled,
-		index = objects.indexOf(object);
+    var objects = this.objects,
+        pooled = this.pooled,
+        index = objects.indexOf(object);
 
-	if (index > -1) {
-		pooled.push(object);
-		objects.splice(index, 1);
-	}
+    if (index > -1) {
+        pooled.push(object);
+        objects.splice(index, 1);
+    }
 
-	return this;
+    return this;
 };
 
 
 ObjectPool.prototype.remove = ObjectPool.prototype.removeObjects = function() {
+    var i = arguments.length;
 
-	for (var i = arguments.length; i--;) this.removeObject(arguments[i]);
-	return this;
+    while (i--) this.removeObject(arguments[i]);
+
+    return this;
 };
 
 
 ObjectPool.prototype.clear = function() {
-	var objects = this.objects,
-		pooled = this.pooled,
-		i;
+    var objects = this.objects,
+        pooled = this.pooled,
+        i = objects.length;
 
-	for (i = objects.length; i--;) pooled.push(objects[i]);
-	objects.length = 0;
+    while (i--) pooled.push(objects[i]);
+    objects.length = 0;
 
-	return this;
+    return this;
+};
+
+
+ObjectPool.prototype.clearForEach = function(fn) {
+    var objects = this.objects,
+        pooled = this.pooled,
+        object,
+        i = objects.length;
+
+    while (i--) {
+        object = objects[i];
+
+        pooled.push(object);
+        fn(object);
+    }
+    objects.length = 0;
+
+    return this;
+};
+
+
+ObjectPool.prototype.empty = function() {
+
+    this.pooled.length = this.objects.length = 0;
+    return this;
 };
 
 
